3f2aad7cfa67e7f955f14e600bc5de325a91a5df
Ricardo Merino <masmerino@gmail.com>
2024-05-01 20:04:32 -0600

    refactor template and add controllers

--- main.go
+++ main.go
@@ -2,57 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"lens.com/m/v2/controllers"
 	"lens.com/m/v2/views"
 )
-
-func execTemplate(w http.ResponseWriter, filePath string) {
-	w.Header().Set("Content-Type", "text/html")
-
-	tpl, err := views.Parse(filePath)
-
-	if err != nil {
-		log.Printf("Error parsin templae %v", err)
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println(views.Msg("el mensaje"))
-
-	tpl.Execute(w, nil)
-}
-
-func homeFunc(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-
-	execTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-
-	execTemplate(w, tplPath)
-}
-
-type Router struct{}
-
-func (Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-
-	switch path {
-	case "/":
-		homeFunc(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Not Found", http.StatusNotFound)
-	}
-}
 
 func main() {
 	r := chi.NewRouter()
@@ -67,17 +24,18 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", homeFunc)
-	r.Get("/contact", contactHandler)
-	r.Route("/products", func(r chi.Router) {
-		r.Get("/{productID}", func(w http.ResponseWriter, r *http.Request) {
-			productID := chi.URLParam(r, "productID")
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
 
-			w.Header().Set("Content-Type", "text/html")
-			fmt.Fprint(w, "<h1>Product here</h1>")
-			fmt.Fprint(w, productID)
-		})
-	})
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not found blah", http.StatusNotFound)
 	})
