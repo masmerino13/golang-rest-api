87f68cfd72eee5d887c651b9cd52f613ddb1936c
Ricardo Merino <masmerino@gmail.com>
2024-05-01 21:41:03 -0600

    implement template must

--- main.go
+++ main.go
@@ -24,16 +24,10 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -11,6 +11,14 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
+
 func Parse(filePath string) (Template, error) {
 	tpl, err := template.ParseFiles(filePath)
 
